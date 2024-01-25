f44c9ae1a69f04581c42a2918f4c8e99c5847c02
Roopesh300 <122351944+Roopesh300@users.noreply.github.com>
2024-01-26 03:58:19 +0530

    Create Request 4.sql

---
+++ Request 4.sql
@@ -0,0 +1,19 @@
+/*1 . Produce a report that calculates the incremental Sold Quantity(ISU%) for each category during the Diwali campaign. Additionally provide rankings for the categories based on their ISU%.
+
+
+with cte as (
+	select 
+	p.category,
+	sum([quantity_sold(before_promo)]) as qty_sold_bp,
+	sum([quantity_sold(after_promo)]) as qty_sold_ap,
+	cast(100 * (sum([quantity_sold(after_promo)]) - sum([quantity_sold(before_promo)])) / (sum([quantity_sold(before_promo)])) as decimal(10,2)) as isu
+	from fact_events_promo f
+	left join [dbo].[dim_products] p on f.product_code = p.product_code
+	where f.campaign_id like 'CAMP_DIW_01'
+	group by p.category)
+
+select 
+category,
+isu as 'isu%',
+rank() over(order by isu desc) as 'rank order'
+from cte
